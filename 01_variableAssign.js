fc072bd84e3a6fa2d8a94c20aebfb10dd6aff311
kanchan-ac <kanchan.choudhari@outlook.com>
2023-12-26 10:59:55 +0530

    Declaration and Initialization variables in JS
    Declaration and Initialization variables in JS

---
+++ 01_variableAssign.js
@@ -0,0 +1,27 @@
+var myFullName;
+console.log(myFullName);
+console.log("****************************************************************************************");
+myFullName="Kanchan Choudhari";
+console.log("My Name is:",myFullName);
+console.log("*****************************************************************************************");
+myFullName="Kanchan Ashish Choudhari";
+console.log("My Full Name is:",myFullName);
+console.log("******************************************************************************************");
+var myLove="I Love only JS";
+console.log(myLove);
+console.log("*******************************************************************************************");
+var firstName="Kanchan";
+var lastName="Choudhari";
+var age="26";
+var address="Kalewadi, Pune"
+var pinCode="411017";
+var gender="Female";
+var isMarried="Yes";
+console.log("First Name",firstName);
+console.log("Last Name:",lastName);
+console.log("Age:",age);
+console.log("Address:",address);
+console.log("Pincode:",pinCode);
+console.log("Gender:",gender);
+console.log("Married:",isMarried);
+console.log("*******************************************************************************");
---
+++ helloWord.js
@@ -0,0 +1,21 @@
+console.log("Hello JS, I am kanchan");
+var num="10";
+console.log(num);
+var myName="Kanchan";
+console.log(myName);
+console.log("First Name:",myName);
+myName="Kanchan Choudhari";
+console.log("Full Name:",myName);
+console.log("Hello JS , I am Gajanan");
+
+var myName; // Varibale Declartion
+console.log(myName); // undefined
+
+myName = "Gajanan"; // Variable Intialization
+console.log("First Name: ",myName);
+
+var age = "32"; // Varibale declaration and Initialization
+console.log("Age:", age);
+
+myName = "Gajanan Kharat"; // Varibale update
+console.log("Full name: ", myName);

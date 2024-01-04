13059ce5dcc8e3e08be43bb72eddf123aaa5bd4f
kanchan-ac <kanchan.choudhari@outlook.com>
2024-01-04 11:54:56 +0530

    Switch case and conditional statement
    Switch case and conditional statement

---
+++ 06_marriageEligibility.js
@@ -0,0 +1,20 @@
+console.log(`-----------------------------------------------Conditional Statement----------------------------------------------------------
+`);
+function marriageEligibility(gender, age) {
+  console.log(`Gender: ${gender}, Age: ${age}`);
+  if (age <= 0 || age == null || age == undefined || isNaN(age)) {
+    console.log(`Sorry! You have entered invalid input\n`);
+  } else {
+    if ((gender == "Male" && age >= 21) || (gender == "Female" && age >= 18)) {
+      console.log(`Hey! you are eligible for marriage\n`);
+    }
+    console.log(`Sorry! You are not eligible for marriage\n`);
+  }
+}
+marriageEligibility("Male", 17);
+marriageEligibility("Male", 25);
+marriageEligibility("Female", 28);
+marriageEligibility("Female", 16);
+marriageEligibility("Other", 35);
+marriageEligibility("Other", 41);
+marriageEligibility("Other", 41);
---
+++ 06_swichCaseMonth.js
@@ -0,0 +1,65 @@
+console.log(
+  `-----------------------------------------------Switch Case Statement----------------------------------------------------------`
+);
+function monthOfYear(month) {
+  switch (month) {
+    case 1:
+      console.log(`Month ${month}: January\n`);
+      break;
+    case 2:
+      console.log(`Month ${month}: February\n`);
+      break;
+
+    case 3:
+      console.log(`Month ${month}: March\n`);
+      break;
+
+    case 4:
+      console.log(`Month ${month}: April\n`);
+      break;
+
+    case 5:
+      console.log(`Month ${month}: May\n`);
+      break;
+
+    case 6:
+      console.log(`Month ${month}: June\n`);
+      break;
+
+    case 7:
+      console.log(`Month ${month}: July\n`);
+      break;
+
+    case 8:
+      console.log(`Month ${month}: August\n`);
+      break;
+
+    case 9:
+      console.log(`Month ${month}: September\n`);
+      break;
+
+    case 10:
+      console.log(`Month ${month}: October\n`);
+      break;
+
+    case 11:
+      console.log(`Month ${month}: November\n`);
+      break;
+
+    case 12:
+      console.log(`Month ${month}: December\n`);
+      break;
+
+    default:
+      console.log(`Month ${month}: Invalid Input\n`);
+      break;
+  }
+}
+monthOfYear(0);
+monthOfYear(2);
+monthOfYear(5);
+monthOfYear(12);
+monthOfYear(15);
+monthOfYear(100);
+monthOfYear(null);
+monthOfYear(undefined);

f44e63bb7d2311b1d7b432d7cb5d9908abb542ce
kanchan-ac <kanchan.choudhari@outlook.com>
2024-01-24 13:47:34 +0530

    DOM Methods
    DOM Methods

---
+++ DOM/14_main.js
@@ -0,0 +1,5 @@
+const elementColor = document.querySelector("#color");
+console.log(elementColor.innerHTML);
+elementColor.style.color = "#A52A2A";
+const elementP = document.querySelector("p");
+elementP.innerHTML = "Project Title: Information Management System";
